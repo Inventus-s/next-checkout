219927181c5e285ac3a426e7f5af0d54bd9124fa
stkmukund <164354566+stkmukund@users.noreply.github.com>
2024-10-01 16:54:36 +0530

    error handling

--- app/components/FormSelect.tsx
+++ app/components/FormSelect.tsx
@@ -5,9 +5,9 @@ const FormSelect = ({ width, options, defaultValue}: { width: string; options: {
   return (
     <select id="countries" className={`bg-gray-50 mt-3 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 ${width}`} >
       <option defaultValue="choose">Choose a {defaultValue}</option>
-      {Array.isArray(options) && options.length && options.map((item) => (
-        <option key={item.price} value="US">{item.name}</option>
-      ))}
+      {/* {Array.isArray(options) && options.length && options.map((item) => (
+        // <option key={item.price} value="US">{item.name}</option>
+      ))} */}
     </select>
   )
 }
